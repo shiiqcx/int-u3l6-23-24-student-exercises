6dfc895887e199834cb49ea92255ee348199a2f1
shiiqcx <149090282+shiiqcx@users.noreply.github.com>
2024-03-07 16:24:47 +0000

    eventsssss

--- events.js
+++ events.js
@@ -2,60 +2,44 @@
 
 
 let hoverCard = document.querySelector('[data-secret="hover"]');
-// 1. Add an event listener to reveal the image on hover.
-//  - The selector is hoverCard
-//  - The event type is mouseover
-{
-    let image = document.querySelector('.hidden-image');
-    image.style.width = '100%';
-    image.style.height = '100%';
-    image.style.opacity = '1';
-}
+hoverCard.addEventListener("mouseover", function() {
+  let image = document.querySelector('.hidden-image');
+  image.style.width = '100%';
+  image.style.height = '100%';
+  image.style.opacity = '1';
+});
 
-// 2. Add an event listener to make the image hidden again.
-//  - The selector is hoverCard
-//  - The event type is mouseout
-{
-    let image = document.querySelector('.hidden-image');
-    image.style.width = '0';
-    image.style.height = '0';
-    image.style.opacity = '0';
-}
+
+hoverCard.addEventListener("mouseout", function() {
+  let image = document.querySelector('.hidden-image');
+  image.style.width = '0';
+  image.style.height = '0';
+  image.style.opacity = '0';
+});
 
 
 
 const clickCard = document.querySelector('[data-secret="click"]');
-// 3. Add an event listener to reveal and animate the shape on click.
-//  - The selector is clickCard
-//  - The event type is click
-{
-    clickCard.classList.toggle('revealed');
-}
+clickCard.addEventListener("click", function() {
+  clickCard.classList.toggle('revealed');
+});
 
 
 const doubleClickCard = document.querySelector('[data-secret="double-click"]');
-// 4. Add an event listener to enlarge the text on double click.
-//  - The selector is doubleClickCard
-//  - The event type is dblclick
-{
-    doubleClickCard.classList.toggle('revealed');
-}
-
+doubleClickCard.addEventListener("dblclick", function() {
+  doubleClickCard.classList.toggle('revealed');
+});
 
 const keypressCard = document.querySelector('[data-secret="keypress"]');
-// 5. Add an event listener to shake the card on any keypress.
-//  - The selector is document
-//  - The event type is keydown
-{
-    keypressCard.classList.add('revealed');
-    keypressCard.style.animation = "shake 0.5s";
-}
+document.addEventListener("keydown", function() {
+  keypressCard.classList.add('revealed');
+  keypressCard.style.animation = "shake 0.5s";
+});
 
 
-// 6. Add an event listener to stop the shaking.
-//  - The selector is document
-//  - The event type is keyup
+
+document.addEventListener("keyup", function ()
 {
-    keypressCard.classList.remove('revealed');
-    keypressCard.style.animation = "";
-}
+  keypressCard.classList.remove('revealed');
+  keypressCard.style.animation = "";
+});
